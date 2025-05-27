6deeda8068399070faa3e366443ff201b46d5aca
GauravKabdal <gauravkabdal10@gmail.com>
2025-05-27 09:02:37 +0530

    added useQuery for fetching the table data

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+
 import StudentTable from "../components/StudentTable";
 import { useNavigate } from "react-router";
+import { useQuery } from "@tanstack/react-query";
 
 function Home() {
   interface student {
@@ -11,25 +12,28 @@ function Home() {
     age: number;
   }
 
-  const [tableData, setTableData] = useState<student[]>([]);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const fetchStudents = async () => {
-      try {
-        const data = await axios.get("http://localhost:5000/api/students");
-        setTableData(data.data);
-        console.log(data.data);
-      } catch (error) {
-        console.log("Cant fetch students..." + error);
-      }
-    };
-    fetchStudents();
-  }, []);
+  const fetchStudents = async (): Promise<student[]> => {
+    const response = await axios.get("http://localhost:5000/api/students");
+    return response.data;
+  };
+
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["studentTableData"],
+    queryFn: fetchStudents,
+  });
+
+  if (error) return <p>There is some error!!</p>;
+
+  if (isLoading) {
+    return <p>Loading....</p>;
+  }
+
   return (
     <>
       <button onClick={() => navigate("/add-student")}>Add New Student</button>
-      <StudentTable studentData={tableData} />
+      {data && <StudentTable studentData={data} />}
     </>
   );
 }
